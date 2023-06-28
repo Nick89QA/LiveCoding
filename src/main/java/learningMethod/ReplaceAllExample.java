bbcd92020139ab8c81493975545a95ad27e17b23
Nickolay Petrych <npetrych@alfabank.ru>
2023-06-28 17:26:23 +0100

    valid Palindrom

--- src/main/java/learningMethod/ReplaceAllExample.java
+++ src/main/java/learningMethod/ReplaceAllExample.java
@@ -6,12 +6,23 @@ public class ReplaceAllExample {
      * "replacement" это цифра или буква на которую хотим заменить
      */
 
+
+    /**
+     * Метод который убирает и возвращает пробелы и другие знаки
+     */
+    public static String isPalindrome(String s) {
+        return s.replaceAll("\\W", "");
+    }
+
     public static void main(String[] args) {
+        String answer =  isPalindrome("fdsf, ,sf sdf,  s,f sf,, s");
+        System.out.println(answer);
+
 
 
         String java = "Строки являются очень важной 111 составляющей java";
 
-        String result = java.replaceAll("С","b");
+        String result = java.replaceAll("С","b"); //заменяем С на b
         System.out.println(result);
     }
 }
