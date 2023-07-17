c66c0eb630bd43149ae6584797e059b2dccd1e3b
Nickolay Petrych <npetrych@alfabank.ru>
2023-07-17 10:02:30 +0100

    add static method

--- src/main/java/stepikCourse/Java/interfacesAndAbstract/InterfaceTest5.java
+++ src/main/java/stepikCourse/Java/interfacesAndAbstract/InterfaceTest5.java
@@ -5,6 +5,9 @@ public class InterfaceTest5 {
 }
 
 interface I1 {
+    static void avv() { //статический метод создали в интерфейсе
+        System.out.println("eto static method avv");
+    }
     void abc();
     default void ghi() {
         System.out.println("это метод ghi");
@@ -20,8 +23,8 @@ class Z2 implements I1 {
         /**
          * имплементировал методы из интерфейса
          * те методы default в которых есть тело,уже не перезапишешь
-         *
          */
+        I1.avv();
         Z2 z = new Z2();
         z.abc();
         z.def();
