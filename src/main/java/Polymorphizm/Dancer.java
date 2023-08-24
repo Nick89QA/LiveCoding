86ea4d367999b9115258cd5a2c2d9e099d60921c
Nickolay Petrych <npetrych@alfabank.ru>
2023-08-24 13:39:08 +0100

    fix

--- src/main/java/Polymorphizm/Dancer.java
+++ src/main/java/Polymorphizm/Dancer.java
@@ -4,10 +4,10 @@ public class Dancer {
     String name;
     int age;
 
-   public Dancer(String name, int age) {
-       this.age = age;
-       this.name = name;
-   }
+    public Dancer(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
 
     public static void main(String[] args) {
         Dancer d1 = new Dancer("Nick", 22);
